4afe893714d432230a0a2be8a09945494dd2ed37
Sarah Dilworth <sarah.petty18@gmail.com>
2018-05-18 15:12:21 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ["snickers" , "hundred grand" , "kitkat" , "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars){
+return ["foo", ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars){
+chocolateBars.unshift("foo");
+return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars){
+return [...chocolateBars, "foo"];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars){
+chocolateBars.push("foo");
+return chocolateBars;
+}
+
+function accessElementInArray(chocolateBars, 
+console.log(chocolateBars 
+}

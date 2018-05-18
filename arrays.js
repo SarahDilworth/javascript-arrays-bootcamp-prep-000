bddde48e1a7d68cdb059577d8ba8edb60aba0bb0
Sarah Dilworth <sarah.petty18@gmail.com>
2018-05-18 15:32:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,7 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
 return chocolateBars.pop();
 }
 
+function removeElementFromEndOfArray(chocolateBars){
+return chocolateBars.slice(0, chocolateBars.length - 1);
+}
+
